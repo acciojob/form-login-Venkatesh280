d530deb67429c1120202eb64d154e74d33914405
yavarna venkatesh <149807257+Venkatesh280@users.noreply.github.com>
2025-02-02 10:11:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 function getFormvalue() {
+	function getFormvalue() {
+    // Get the values of the first and last name input fields
+    var firstName = document.getElementById("firstName").value;
+    var lastName = document.getElementById("lastName").value;
+    
+    // Combine first and last name
+    var fullName = firstName + " " + lastName;
+    
+    // Display the full name using alert
+    alert(fullName);
+}
+
     //Write your code here
 
 }
